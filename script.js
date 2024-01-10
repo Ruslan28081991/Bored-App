8c5083aab6404f3201aa68334974c3063dfdfbad
Ruslan <vich_91@mail.ru>
2024-01-10 20:41:36 +0500

    create Bored-App

---
+++ script.js
@@ -0,0 +1,24 @@
+const btnNode = document.querySelector(".btn");
+
+const addText = () => {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((data) => data.json())
+    .then((res) => {
+      const textAPI = res.activity;
+      document.querySelector(".text").innerHTML = `<p>${textAPI}</p> `;
+    });
+  changeStyle();
+  changeTitle();
+};
+
+const changeStyle = () => {
+  const containerNode = document.querySelector(".container");
+  containerNode.classList = "green";
+};
+
+const changeTitle = () => {
+  const titleNode = document.querySelector(".title");
+  titleNode.innerHTML = "Ура, теперь не скучно 🔥";
+};
+
+btnNode.addEventListener("click", addText);
